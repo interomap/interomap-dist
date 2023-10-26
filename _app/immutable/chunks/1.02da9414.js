3281ead5c8be622a344726da06804c43268ccccf
Joey Khalil <mokhito@users.noreply.github.com>
2023-10-26 10:49:49 +0200

    refactor: add scale factor to serialized output

---
+++ _app/immutable/chunks/1.02da9414.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.14b99c83.js";export{t as component};
--- _app/immutable/chunks/1.608cf4de.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.1e5778bd.js";export{t as component};
